54b910e720d3c3f381c5640e6010084b48192f48
AGena  tork <m2117513495@gmail.com>
2026-03-28 02:28:27 +0200

    Update main.ino
    Signed-off-by: AGena  tork <m2117513495@gmail.com>

--- main.ino
+++ main.ino
@@ -10,3 +10,24 @@ void loop() {
   Serial.println(value); 
   delay(1000);
 }
+#include <ESP8266WiFi.h>
+
+const char* ssid = "YourNetworkName";
+const char* password = "YourPassword";
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.println("Connected to WiFi!");
+}
+
+void loop() {
+  // هنا تضيف كود الكشف عن الدخان
+  // لو فيه دخان، ابعت تنبيه عبر HTTP أو MQTT
+}
